36648e700dc023f6449d68867915a23de02a938d
Govind M J <govindmj2002@gmail.com>
2023-04-22 03:16:22 +0530

    added send message

---
+++ send-email.php
@@ -0,0 +1,17 @@
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = 'govindmj2002@gmail.com';
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    if(mail($to, $subject, $message, $headers)) {
+        echo '<p>Your message has been sent.</p>';
+    } else {
+        echo '<p>Sorry, there was an error sending your message.</p>';
+    }
+}
